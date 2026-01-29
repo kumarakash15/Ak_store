aaca8ccbf52bf400400fe7e4945fdde022e0a0af
kumarakash15 <akashsahoo31538@gmail.com>
2026-01-29 17:35:04 +0530

    some file added

--- init/index.js
+++ init/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const items = require("./data");
-const { Listing } = require("../models/listing");
+const { Listing, Cart } = require("../models/listing");
+const Order = require("../models/order");
 const mongo_url = "mongodb://127.0.0.1:27017/akstore";
 
 main().then(() => {
@@ -15,6 +16,8 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    await Cart.deleteMany({});
+    await Order.deleteMany({});
     await Listing.insertMany(items);
     console.log("Data initialized successfully");
 }
